608a55084730168bbbe0be4e2c980913de063b5b
Dineshkumar-318 <gandidinesh318@gmail.com>
2025-05-20 13:24:36 +0530

    Add files via upload

---
+++ DAY12/palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pallindromestring {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s = sc.nextLine();
+        String pallindrome="";
+        char tar= s.charAt(0); ;
+        for(int i=s.length()-1;i>=0;i--){
+            pallindrome+=s.charAt(i);
+        }
+        if(s.compareToIgnoreCase(pallindrome) == 0){
+            System.out.println("The string is a palindrome");
+        }
+        else{
+            System.out.println("The string is not a palindrome");
+        }
+ }
+}
