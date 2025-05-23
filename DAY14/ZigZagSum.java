05944488ccefb928711a6f93823bfeb7d2e26034
Dineshkumar-318 <gandidinesh318@gmail.com>
2025-05-23 09:49:31 +0530

    Add files via upload

---
+++ DAY14/ZigZagSum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ZigZagSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix[i][j] = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < cols; j++)
+                    sum += matrix[i][j];
+            } else {
+                for (int j = cols - 1; j >= 0; j--)
+                    sum += matrix[i][j];
+            }
+        }
+
+        System.out.println("Zig-zag sum: " + sum);
+
+        sc.close();
+    }
+}
